a87c27840aa0f8122ce0fb271a90bfb3bb74f61a
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-03-29 13:05:58 +0500

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,7 +1,6 @@
 immutable_var = 1, 2, 3, 4, "table"
 print(immutable_var)
-#immutable_var[0] = 2 кортеж – это неизменяемая упорядоченная
-# коллекция, которая может содержать в себе разные типы данных.
+#immutable_var[0] = 2 кортеж – это неизменяемая упорядоченная коллекция, которая может содержать в себе разные типы данных.
 mutable_list = ['banana', 'apple', 'tea']
 print(mutable_list)
 mutable_list[0]= 'coconut'
