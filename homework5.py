ded150b15bd6e7f5c7e23d745d3a7aefa59f324e
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-03-30 14:00:28 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi','grape']
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'coconut'
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин', 'grape': 'виноград'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['coconut']='кокос'
+print(my_dict)
